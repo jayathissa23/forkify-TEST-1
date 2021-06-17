0bb6cf2db707f0b25d55be13fce287b56faabe04
NIRMITHA <jayathissaara@gmail.com>
2021-06-17 18:32:38 +0000

    Initiol commit

---
+++ script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let buttons=document.querySelectorAll('.show-modal');
+let closeBtn=document.querySelector('.close-modal');
+
+let showOverlay=document.querySelector('.overlay');
+let showModel=document.querySelector('.modal');
+
+
+
+
+
+let closeModel=function() {
+    showModel.classList.add('hidden');
+    showOverlay.classList.add('hidden');
+}
+
+let openModel=function() {
+    showModel.classList.remove('hidden');
+       showOverlay.classList.remove('hidden');
+}
+
+for (let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('click',function(){
+       openModel();
+    });
+};
+
+closeBtn.addEventListener('click',function(e) {
+    e.preventDefault();
+    closeModel();
+});
+
+
+document.addEventListener('keydown',function(e) {
+    e.preventDefault();
+    console.log(e.key);
+    if(e.key==='Escape') closeModel();
+});
+
+
+showOverlay.addEventListener('click',function(e) {
+    e.preventDefault();
+    closeModel();
+});
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
